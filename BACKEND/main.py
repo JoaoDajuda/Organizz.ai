f5657b9795f49ca75e45d393e0433c5b2f6adcb1
Joao Henrique <jh5777720@gmail.com>
2026-07-24 21:27:00 -0300

    Rota de soma concluída e funcionando com o banco de dados

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -4,7 +4,8 @@ import os
 app = FastAPI()
 
 from auth_routes import auth_router
-from math_routes import math_router
+from math_routes import math_router, add_router
 
 app.include_router(auth_router)
 app.include_router(math_router)
+app.include_router(add_router)
