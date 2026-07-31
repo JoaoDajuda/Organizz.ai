166a8134053676c4bcd714cbbeb0de1200a57c76
Joao <joaopssoares607@gmail.com>
2026-07-31 19:44:04 -0300

    Alteração para tailwind

--- FRONTEND/web/src/main.jsx
+++ FRONTEND/web/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './pages/Login'
-import Financas from './pages/Financas'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Financas />
+    <Login />
   </StrictMode>,
 )
