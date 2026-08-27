a33b255fe733161c787fee5a09ee64e3fd1fb601
Pbo_moraes <pablomoraesdesouza2020@gmail.com>
2026-08-27 20:32:09 -0300

    Arrumando os inputs do math_routes no banco de dados

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -2,6 +2,6 @@ from sqlmodel import create_engine
 
 # Mesma URL usada no alembic.ini (sqlalchemy.url).
 # Se um dia mudar usuário/senha/host do banco, troque aqui E no alembic.ini.
-DATABASE_URL = "mysql+pymysql://root:201ab@localhost:3306/organizzai"
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/organizzai"
 
 engine = create_engine(DATABASE_URL, echo=True)
