c3a3f91e36a898c7471086fcc1f7102c46ede640
Pbo_moraes <pablomoraesdesouza2020@gmail.com>
2026-08-27 22:14:15 -0300

    Add campo 'saldo' na tabela Valores

--- BACKEND/models.py
+++ BACKEND/models.py
@@ -77,6 +77,7 @@ class Valores(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     id_usuario: int = Field(foreign_key="usuario.id_usuario", index=True)
     valor: float = Field()
+    saldo: Optional[float] = Field(default=0.0)
     data_movimentacao: datetime = Field(default_factory=datetime.utcnow)
 
     usuario: Optional[Usuario] = Relationship(back_populates="valores")
